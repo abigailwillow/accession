06138b9aa7728c85d73619988d64eb1acee69244
Abigail de Joode <abbydiode@gmail.com>
2022-08-02 18:34:29 +0200

    Add Vector extensions for easier Vector2 to Vector3 conversion

--- Assets/Scripts/BoardController.cs
+++ Assets/Scripts/BoardController.cs
@@ -2,17 +2,17 @@ using UnityEngine;
 
 public class BoardController : MonoBehaviour {
     [Tooltip("The coordinates of the bottom left corner of the board in local space.")]
-    [SerializeField] private Vector3 bottomLeftCorner;
+    [SerializeField] private Vector2 bottomLeftCorner;
     /// <summary>
     /// The coordinates of the bottom left corner of the board in local space.
     /// </summary>
-    public Vector3 BottomLeftCorner { get => bottomLeftCorner + transform.position; }
+    public Vector3 BottomLeftCorner { get => bottomLeftCorner.XZ() + transform.position; }
     [Tooltip("The coordinates of the top right corner of the board in local space.")]
-    [SerializeField] private Vector3 topRightCorner;
+    [SerializeField] private Vector2 topRightCorner;
     /// <summary>
     /// The coordinates of the top right corner of the board in local space.
     /// </summary>
-    public Vector3 TopRightCorner { get => topRightCorner + transform.position; }
+    public Vector3 TopRightCorner { get => topRightCorner.XZ() + transform.position; }
     [Tooltip("The amount of rows and columns on the board.")]
     [SerializeField] private Vector2Int gridSize = new Vector2Int(8, 8);
     [SerializeField] private GameObject piecePrefab;
@@ -30,16 +30,16 @@ public class BoardController : MonoBehaviour {
 
     private void Awake() {
         grid = new Transform[gridSize.x, gridSize.y];
-        Vector3 distance = TopRightCorner - BottomLeftCorner;
-        Vector3 cellSize = new Vector3(distance.x / gridSize.x, 0, distance.z / gridSize.y);
-        Vector3 cellOffset = cellSize / 2;
+        Vector3 boardSize = TopRightCorner - BottomLeftCorner;
+        Vector3 cellSize = new Vector3(boardSize.x / gridSize.x, 0, boardSize.z / gridSize.y);
+        Vector3 offset = cellSize / 2;
         for (int x = 0; x < gridSize.x; x++) {
             for (int y = 0; y < gridSize.y; y++) {
-                Vector3 position = new Vector3(bottomLeftCorner.x + cellSize.x * x, 0, bottomLeftCorner.z + cellSize.z * y) + cellOffset;
-                GameObject cell = new GameObject($"Cell ({x}, {y})");
-                cell.transform.position = position;
-                cell.transform.SetParent(transform);
-                Instantiate(piecePrefab, cell.transform, false);
+                Vector3 position = new Vector3(BottomLeftCorner.x + cellSize.x * x, 0, BottomLeftCorner.z + cellSize.z * y) + offset;
+                GameObject cell = Instantiate(cellPrefab, position, Quaternion.identity, transform);
+                GameObject piece = Instantiate(piecePrefab, cell.transform, false);
+                cell.name = $"Cell ({x}, {y})";
+                piece.name = $"Piece ({x}, {y})";
                 grid[x, y] = cell.transform;
             }
         }
@@ -48,13 +48,12 @@ public class BoardController : MonoBehaviour {
     private void OnDrawGizmosSelected() {
         Vector3 boardSize = TopRightCorner - BottomLeftCorner;
         Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(transform.position, new Vector3(boardSize.x, 0, boardSize.z));
+        Gizmos.DrawWireCube(transform.position, boardSize);
 
         if (grid != null) {
-            Gizmos.color = Color.blue;
             for (int x = 0; x < grid.GetLength(0); x++) {
                 for (int y = 0; y < grid.GetLength(1); y++) {
-                    Gizmos.DrawRay(grid[x, y].position, Vector3.up * 0.05f);
+                    Gizmos.DrawWireCube(grid[x, y].position, new Vector3(0.1f, 0.02f, 0.1f));
                 }
             }
         }
