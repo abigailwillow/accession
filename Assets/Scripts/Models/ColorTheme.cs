397fa0838be7fdbf51aea57be19e1e2ade41c550
Abigail de Joode <abbydiode@gmail.com>
2022-08-09 16:54:41 +0200

    Add PieceColor enum to help distinguish colors

--- Assets/Scripts/Models/ColorTheme.cs
+++ Assets/Scripts/Models/ColorTheme.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Color Theme", menuName = "Accession/Color Theme", order = 0)]
@@ -23,4 +22,36 @@ public class ColorTheme : ScriptableObject {
     public Cell cell;
 
     public Color validMove;
+
+    public Color AddColors(PieceColor a, PieceColor b) => (a, b) switch {
+        (PieceColor.Red, PieceColor.Red) => piece.red,
+        (PieceColor.Red, PieceColor.Green) => piece.yellow,
+        (PieceColor.Red, PieceColor.Blue) => piece.magenta,
+        (PieceColor.Green, PieceColor.Red) => piece.yellow,
+        (PieceColor.Green, PieceColor.Green) => piece.green,
+        (PieceColor.Green, PieceColor.Blue) => piece.cyan,
+        (PieceColor.Blue, PieceColor.Red) => piece.magenta,
+        (PieceColor.Blue, PieceColor.Green) => piece.cyan,
+        (PieceColor.Blue, PieceColor.Blue) => piece.blue,
+        _ => Color.white
+    };
+
+    public Color GetPieceColor(PieceColor color) => color switch {
+        PieceColor.Red => piece.red,
+        PieceColor.Green => piece.green,
+        PieceColor.Blue => piece.blue,
+        PieceColor.Magenta => piece.magenta,
+        PieceColor.Yellow => piece.yellow,
+        PieceColor.Cyan => piece.cyan,
+        _ => Color.white
+    };
+
+    public enum PieceColor {
+        Red,
+        Green,
+        Blue,
+        Magenta,
+        Yellow,
+        Cyan
+    }
 }
