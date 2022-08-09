ece65fbd67f1a73524513e8482db81e278282694
Abigail de Joode <abbydiode@gmail.com>
2022-08-09 17:11:14 +0200

    Put piece-related methods and values under the piece struct

--- Assets/Scripts/Models/ColorTheme.cs
+++ Assets/Scripts/Models/ColorTheme.cs
@@ -4,12 +4,55 @@ using UnityEngine;
 public class ColorTheme : ScriptableObject {
     [System.Serializable]
     public struct Piece {
-        public Color red;
-        public Color green;
-        public Color blue;
-        public Color magenta;
-        public Color yellow;
-        public Color cyan;
+        public UnityEngine.Color red;
+        public UnityEngine.Color green;
+        public UnityEngine.Color blue;
+        public UnityEngine.Color magenta;
+        public UnityEngine.Color yellow;
+        public UnityEngine.Color cyan;
+
+        /// <summary>
+        /// Converts a PieceColor to a Color.
+        /// </summary>
+        /// <param name="color">The PieceColor</param>
+        /// <returns></returns>
+        public UnityEngine.Color GetColor(Piece.Color color) => color switch {
+            Piece.Color.Red => red,
+            Piece.Color.Green => green,
+            Piece.Color.Blue => blue,
+            Piece.Color.Magenta => magenta,
+            Piece.Color.Yellow => yellow,
+            Piece.Color.Cyan => cyan,
+            _ => UnityEngine.Color.white
+        };
+
+        /// <summary>
+        /// Statically adds the two colors together and returns the result as a color.
+        /// </summary>
+        /// <param name="a">The instigating piece color.</param>
+        /// <param name="b">The target piece color.</param>
+        /// <returns>The combined color.</returns>
+        public UnityEngine.Color Add(Piece.Color a, Piece.Color b) => (a, b) switch {
+            (Piece.Color.Red, Piece.Color.Red) => red,
+            (Piece.Color.Red, Piece.Color.Green) => yellow,
+            (Piece.Color.Red, Piece.Color.Blue) => magenta,
+            (Piece.Color.Green, Piece.Color.Red) => yellow,
+            (Piece.Color.Green, Piece.Color.Green) => green,
+            (Piece.Color.Green, Piece.Color.Blue) => cyan,
+            (Piece.Color.Blue, Piece.Color.Red) => magenta,
+            (Piece.Color.Blue, Piece.Color.Green) => cyan,
+            (Piece.Color.Blue, Piece.Color.Blue) => blue,
+            _ => UnityEngine.Color.white
+        };
+
+        public enum Color {
+            Red,
+            Green,
+            Blue,
+            Magenta,
+            Yellow,
+            Cyan
+        }
     }
 
     [System.Serializable]
@@ -22,36 +65,4 @@ public class ColorTheme : ScriptableObject {
     public Cell cell;
 
     public Color validMove;
-
-    public Color AddColors(PieceColor a, PieceColor b) => (a, b) switch {
-        (PieceColor.Red, PieceColor.Red) => piece.red,
-        (PieceColor.Red, PieceColor.Green) => piece.yellow,
-        (PieceColor.Red, PieceColor.Blue) => piece.magenta,
-        (PieceColor.Green, PieceColor.Red) => piece.yellow,
-        (PieceColor.Green, PieceColor.Green) => piece.green,
-        (PieceColor.Green, PieceColor.Blue) => piece.cyan,
-        (PieceColor.Blue, PieceColor.Red) => piece.magenta,
-        (PieceColor.Blue, PieceColor.Green) => piece.cyan,
-        (PieceColor.Blue, PieceColor.Blue) => piece.blue,
-        _ => Color.white
-    };
-
-    public Color GetPieceColor(PieceColor color) => color switch {
-        PieceColor.Red => piece.red,
-        PieceColor.Green => piece.green,
-        PieceColor.Blue => piece.blue,
-        PieceColor.Magenta => piece.magenta,
-        PieceColor.Yellow => piece.yellow,
-        PieceColor.Cyan => piece.cyan,
-        _ => Color.white
-    };
-
-    public enum PieceColor {
-        Red,
-        Green,
-        Blue,
-        Magenta,
-        Yellow,
-        Cyan
-    }
 }
