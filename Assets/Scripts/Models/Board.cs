11fc33722c600e3b234b289aaa149317d9a7472b
Abigail de Joode <abbydiode@gmail.com>
2022-08-12 18:08:55 +0200

    Create board model and basic (de)serialization

---
+++ Assets/Scripts/Models/Board.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace Accession {
+    public class Board {
+        public Vector2Int size { get; private set; }
+        public List<Piece> pieces { get; private set; } = new List<Piece>();
+
+        
+        public Board(Vector2Int size, List<Piece> pieces = null) {
+            this.size = size; 
+            this.pieces = pieces;        
+        }
+
+        /// <summary>
+        /// Serializes this board to a string.
+        /// </summary>
+        /// <returns>This board as a serialized string.</returns>
+        public string Serialize() => JsonUtility.ToJson(this, true);
+
+        /// <summary>
+        /// Creates a new board from the given file.
+        /// </summary>
+        /// <param name="path">The path to the file to deserialize.</param>
+        /// <returns>A new board object.</returns>
+        public static Board Deserialize(string path) => JsonUtility.FromJson<Board>(File.ReadAllText(path));
+    }
+}
