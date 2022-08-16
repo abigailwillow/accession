8bdeeba30d7054c8100702c37e241832fd4c0e83
Abigail de Joode <abbydiode@gmail.com>
2022-08-16 21:25:09 +0200

    Add settings panel

--- Assets/Scripts/Controllers/UI/SettingsController.cs
+++ Assets/Scripts/Controllers/UI/SettingsController.cs
@@ -1,18 +1,21 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Localization;
 using UnityEngine.Localization.Settings;
 using TMPro;
 
 namespace Accession.Controllers {
     public class SettingsController : MonoBehaviour {
         [SerializeField] private TMP_Dropdown dropdown;
+        private List<Locale> locales = LocalizationSettings.AvailableLocales.Locales;
         private void Awake() {
-            LocalizationSettings.AvailableLocales.Locales.ForEach(locale => {
+            this.locales.ForEach(locale => {
                 dropdown.options.Add(new TMP_Dropdown.OptionData(locale.LocaleName));
             });
         }
 
-        public void OnLocaleChanged(TMP_Dropdown dropdown) {
-            Debug.Log($"Locale changed to {dropdown.value}");
+        public void OnLocaleChanged(int option) {
+            Debug.Log($"Locale changed to {locales[option].LocaleName}");
         }
 
         public void SwitchPanel(GameObject panel) {
