43e8057985026372689fff966844be1584a1acef
Abigail de Joode <abbydiode@gmail.com>
2022-08-17 19:59:57 +0200

    Make level complete panel pop up

--- Assets/Scripts/Controllers/UI/NextLevelController.cs
+++ Assets/Scripts/Controllers/UI/NextLevelController.cs
@@ -5,9 +5,12 @@ namespace Accession.Controllers {
     public class NextLevelController : MonoBehaviour {
         [SerializeField] GameObject panel;
 
-        private void Awake() => BoardController.instance.onBoardCompleted += OnBoardCompleted;
+        private void Awake() {
+            BoardController.instance.onBoardCompleted += OnBoardCompleted;
+            this.panel.SetActive(false);
+        }
 
-        private void OnBoardCompleted() => panel.SetActive(true);
+        private void OnBoardCompleted() => this.panel.SetActive(true);
 
         public void OnNextLevelClicked() {
             // TODO: Save progress to retrieve latest completed level and get next level;
